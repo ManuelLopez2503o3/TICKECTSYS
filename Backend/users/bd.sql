6d9805ab33b772b1ef97fdca0ad535813580e249
Alejandro <ml1549474@gmail.com>
2026-05-26 14:01:56 -0600

    Agrega backend y base de datos

---
+++ Backend/users/bd.sql
@@ -0,0 +1,5 @@
+create database TICKECTS;
+Create table users;
+insert into users values (1, 'John', 'Doe', 'john.doe@example.com');
+insert into users values (2, 'Jane', 'Smith', 'jane.smith@example.com');
+
